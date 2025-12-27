5950b7187ff5a99014e4e058e05a11aa2d4b21ab
Manish <mkthakur2301@gmail.com>
2025-12-27 14:56:40 +0530

    feat: add instructorRole based authentication

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
-
+import instructorRoutes from "./routes/instructorRoutes.js"
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/instructor", instructorRoutes);
 
 app.get("/", (req, res )=>{
     res.send("SkillBridge API running");
