938ab4d480d61e7b781982a94a91737b7d428954
Manish <mkthakur2301@gmail.com>
2025-12-30 19:49:06 +0530

    feat: register enrollmentRoutes in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import instructorRoutes from "./routes/instructorRoutes.js"
 import adminRoutes from "./routes/adminRoutes.js";
-import courseRoutes from "./routes/courseRoutes.js"
+import courseRoutes from "./routes/courseRoutes.js";
+import enrollmentRoutes from "./routes/enrollmentRoutes.js";
 const app = express();
 
 app.use(cors());
@@ -13,6 +14,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/instructor", instructorRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/courses", courseRoutes);
+app.use("/api/enrollments", enrollmentRoutes);
 
 app.get("/", (req, res )=>{
     res.send("SkillBridge API running");
