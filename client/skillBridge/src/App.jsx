9ec2c31a894d1a56e34658829f182a89bdbfd01a
Manish <mkthakur2301@gmail.com>
2026-01-03 00:05:18 +0530

    setup: Routing setup

--- client/skillBridge/src/App.jsx
+++ client/skillBridge/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import MainLayout from "./layouts/MainLayout";
 
-const App = () => {
+function App() {
   return (
-    <div className=''>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <MainLayout>
+                <Dashboard />
+              </MainLayout>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
